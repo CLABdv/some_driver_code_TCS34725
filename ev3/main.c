5f942d50a87e3c2957de86f147b13f773c30a478
Casper Beckman <casper@debstation.home>
2023-10-02 00:14:51 +0200

    nothing frigging wroks AHHHHHHHHHHHHHH

--- ev3/main.c
+++ ev3/main.c
@@ -8,7 +8,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include "main.h"
@@ -17,50 +16,49 @@ int main()
 {
     int file;
     char filename[] = "/dev/i2c-3"; 
-    char buf[100];
-    unsigned char *ubuf=(unsigned char *)buf;
     if ((file = open(filename, O_RDWR)) < 0)
     {
-        strerror_r(errno, buf, 100);
-        fprintf(stderr, "error: failed to open bus\n"
-                "err: %s\n", buf);
+        fprintf(stderr, "error: failed to open bus\n%s\n", strerror(errno));
         exit(1);
-    }
-    /*
-    // initiate communication
-    if (ioctl(file, I2C_SLAVE, TCS34725_ADDR) < 0)
-    {
-        strerror_r(errno, buf, 100);
-        fprintf(stderr, "error: failed to acquire bus acces and/or talk to slave\n"
-                "err: %s\n", buf);
-        close(file);
-        exit(1);
-    }
-    unsigned char dat = CMD_BIT | AUTO_INC_TRANSAC | 0x12; // 0x12 is id register
-    if(write(file, &dat, 1) != 1)
-    {
-        fprintf(stderr, "didnt write 1 byte\n");
     }
 
-    if (read(file, ubuf, 1) != 1)
-    {
-        fprintf(stderr, "didnt read 1 byte\n");
-    }
-    else
-    {
-        printf("read %x\n", *ubuf);
-    }
-    */
+
+    /*
     unsigned long funcs = 0;
     ioctl(file, I2C_FUNCS, &funcs);
     if(funcs & I2C_FUNC_I2C)
     {
         printf("supports I2C!\n");
     }
+    */
+    
+    if(ioctl(file, I2C_SLAVE, TCS34725_ADDR) < 0)
+    {
+        fprintf(stderr, "error: Could not ioctl I2C_SLAVE;\n%s\n", strerror(errno));
+        close(file);
+        exit(1);
+    }
+
+    __u8 reg = CMD_BIT | AUTO_INC_TRANSAC;
+    __u8 dat = 0xA;
+    __u8 datbuf[] = {reg, dat};
+    if (write(file, datbuf, 1) != 1)
+    {
+        fprintf(stderr, "error writing (AT FIRST WRITE);\n%s\n", strerror(errno));
+        close(file);
+        exit(1);
+    }
+    if (write(file, datbuf+1, 1) != 1)
+    {
+        fprintf(stderr, "error writing (AT SECOND WRITE);\n%s\n", strerror(errno));
+        close(file);
+        exit(1);
+    }
     else
     {
-        fprintf(stderr, "does not support i2c stuffy\n");
-    }
+        printf("wrote some shit!\n");
+    };
+
 
     close(file);
     return 0;
